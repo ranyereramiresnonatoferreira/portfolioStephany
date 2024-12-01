0cfc0624218f594023588f5ce92eb9f5d4e47d75
ranyereramires <ranyereramiresg@hotmail.com>
2024-11-30 21:29:51 -0300

    seguindo com a tela de trabalhos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { WorksComponent } from './pages/works/works.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent }
+  { path: 'works', component: WorksComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ManubarComponent } from './components/manubar/manubar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { WorksComponent } from './pages/works/works.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ManubarComponent,
-    HomeComponent
+    HomeComponent,
+    WorksComponent
   ],
   imports: [
     BrowserModule,
